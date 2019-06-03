b0d63fa6aa461b9c5bbca119fa092505d167028c
Alice Kim <alice828@mail.ubc.ca>
2019-06-03 11:37:21 -0700

    Clean up code

--- lib/main.dart
+++ lib/main.dart
@@ -3,25 +3,15 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-//      home: MyHomePage(title: 'Flutter Demo Home Page'),
-      home: new Button(),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+//      home: new Button(),
     );
   }
 }
@@ -34,12 +24,17 @@ class FormulaRoute extends StatelessWidget {
         title: Text("Formula Route"),
       ),
       body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text('Go back!'),
-        ),
+          child: Column(
+            children: <Widget>[
+              new Padding(padding: new EdgeInsets.only(bottom: 30.0)),
+              new RaisedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('Go back!'),
+              ),
+            ],
+          )
       ),
     );
   }
@@ -67,15 +62,6 @@ class ProblemRoute extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -83,79 +69,15 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
   void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+//    setState(() {
+//      _counter++;
+//    });
+    print("Floating button pressed");
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
-
-class Button extends StatefulWidget {
-  @override
-  ButtonState createState() => new ButtonState();
-}
-
-class ButtonState extends State<Button> {
-  int counter = 0;
+//  int counter = 0;
 
   void choiceAction(Constant choice) {
     print(choice.toString().substring(9));
@@ -163,12 +85,34 @@ class ButtonState extends State<Button> {
 
   List<Constant> choices = <Constant> [Constant.Logout, Constant.Settings, Constant.Sound];
 
+  List<String> items = <String> [];
+  TextEditingController controller = new TextEditingController();
+  String filter;
+
+  @override
+  initState() {
+    items.add("Apple");
+    items.add("Bananas");
+    items.add("Milk");
+    items.add("Water");
+    controller.addListener(() {
+      setState(() {
+        filter = controller.text;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("Math App"),
-        backgroundColor: Colors.blue,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
         actions: <Widget>[
           PopupMenuButton<Constant>(
             onSelected: choiceAction,
@@ -183,37 +127,64 @@ class ButtonState extends State<Button> {
           )
         ],
       ),
-      body: new Container(
-        child: new Center(
-          child: new Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              new Text("Title", style: new TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold)),
-              new Padding(padding: new EdgeInsets.only(bottom: 50.0)),
-              new RaisedButton(
-                  child: new Text("Formula", style: new TextStyle(color: Colors.white, fontSize: 20.0)),
-                  color: Colors.blueAccent,
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => FormulaRoute()),
-                    );
-                  }
+      body: Center(
+        child: Column(
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Text("Title", style: new TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold)),
+            new Expanded(
+              child: new TextField(
+                decoration: new InputDecoration(
+                    labelText: "Search something"
+                ),
+                controller: controller,
               ),
-              new Padding(padding: new EdgeInsets.all(10.0)),
-              new RaisedButton(
-                  child: new Text("Problem", style: new TextStyle(color: Colors.white, fontSize: 20.0)),
-                  color: Colors.blueAccent,
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => ProblemRoute()),
-                    );
-                  }
-              )
-            ],
-          ),
+            ),
+            new Expanded(
+              child: new ListView.builder(
+                itemCount: items.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return filter == null || filter == "" ? new Card(child: new Text(items[index])) : items[index].toLowerCase().contains(filter.toLowerCase()) ? new Card(child: new Text(items[index])) : new Container();
+                },
+              ),
+            ),
+            new Padding(padding: new EdgeInsets.only(bottom: 50.0)),
+            new RaisedButton(
+              child: new Text("Formula", style: new TextStyle(color: Colors.white, fontSize: 20.0)),
+              color: Colors.blueAccent,
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => FormulaRoute()),
+                );
+              },
+              padding: new EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+            ),
+            new Padding(padding: new EdgeInsets.all(10.0)),
+            new RaisedButton(
+              child: new Text("Problem", style: new TextStyle(color: Colors.white, fontSize: 20.0)),
+              color: Colors.blueAccent,
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ProblemRoute()),
+                );
+              },
+              padding: new EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+            ),
+            new Padding(padding: new EdgeInsets.only(bottom: 50.0)),
+            new Text("Made with ♥️ by Team")
+          ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
       ),
     );
   }
