1a15cfed83c0b1813ee17de5756c3a8c5614c1f4
Alice Kim <alice828@mail.ubc.ca>
2019-06-08 18:24:35 -0700

    Apply design to Problem page

--- lib/pages.dart
+++ lib/pages.dart
@@ -6,34 +6,45 @@ enum Choice {
   Settings, Sound, Logout
 }
 
+enum Level {
+  K8, High, Univ, SAT
+}
+
 int _id = 0;
+
+List<Item> items = [];
+//List<Tag>  tags  = [];
+
 List<Choice> _choices = [Choice.Logout, Choice.Settings, Choice.Sound];
 
 List<PopupMenuItem<Choice>> popupMenuItems() => _choices.map((choice) { return PopupMenuItem<Choice>(value: choice, child: Text(choice.toString().split('.').last),);}).toList();
+
 void choiceAction(Choice choice) => print(choice.toString().split('.').last);
+
 PopupMenuButton<Choice> popupMenuButton() => PopupMenuButton<Choice>(onSelected: choiceAction, itemBuilder: (BuildContext context) { return popupMenuItems(); },);
 
 class Item extends Comparable {
   int id;
   String title;
+  Level level;
   bool active;
-  List<String> tags = [];
+  List<Tag> tags = [];
 
-  Item(this.title) {
+  Item(this.title, this.level) {
     this.id = _id++;
     this.active = true;
   }
 
-  void addTag(String tag) {
+  void addTag(Tag tag) {
     tags.add(tag);
   }
 
-  bool containsTag(String tag) {
+  bool containsTag(Tag tag) {
     return tags.contains(tag);
   }
 
   bool isActive(List<Tag> activeTags) {
-    return activeTags.any((tag) => tags.contains(tag.title));
+    return activeTags.any((tag) => tags.contains(tag));
   }
 
   @override
@@ -73,7 +84,7 @@ class _LevelsPageState extends State<LevelsPage> {
                   RaisedButton(
                     padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
                     onPressed: () {
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => LevelRoute(title: "K - 8th Grade", tag: "K8")));
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => LevelRoute(title: "K - 8th Grade", tag: "K8", level: Level.K8,)));
                     },
                     child: Column(
                       children: <Widget>[
@@ -89,7 +100,7 @@ class _LevelsPageState extends State<LevelsPage> {
                   RaisedButton(
                     padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
                     onPressed: () {
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => LevelRoute(title: "9 - 12th Grade", tag: "912")));
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => LevelRoute(title: "9 - 12th Grade", tag: "912", level: Level.High,)));
                     },
                     child: Column(
                       children: <Widget>[
@@ -111,7 +122,7 @@ class _LevelsPageState extends State<LevelsPage> {
                   RaisedButton(
                     padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
                     onPressed: () {
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => LevelRoute(title: "SAT", tag: "SAT")));
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => LevelRoute(title: "SAT", tag: "SAT", level: Level.SAT,)));
                     },
                     child: Column(
                       children: <Widget>[
@@ -127,7 +138,7 @@ class _LevelsPageState extends State<LevelsPage> {
                   RaisedButton(
                     padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
                     onPressed: () {
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => LevelRoute(title: "University", tag: "Univ")));
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => LevelRoute(title: "University", tag: "Univ", level: Level.Univ,)));
                     },
                     child: Column(
                       children: <Widget>[
@@ -160,51 +171,74 @@ class BrowsePage extends StatefulWidget {
 }
 
 class _BrowsePageState extends State<BrowsePage> {
-  List<Item>     items       = [];
-  List<Item>     activeItems = [];
-  List<Tag>      tags        = [];
+//  List<Item> items = [];
+  List<Tag>  tags  = [];
 
   TextEditingController controller = TextEditingController();
   String filter;
 
-  InkWell _inkWell(Card card) {
-    return InkWell(onTap: (){ Navigator.push(context, MaterialPageRoute(builder: (context) => ProblemRoute())); }, child: card);
-  }
+//  InkWell _inkWell(Card card) {
+//    return InkWell(onTap: (){ Navigator.push(context, MaterialPageRoute(builder: (context) => ProblemRoute())); }, child: card);
+//  }
 
   Card _card(int index) {
-    return Card(child: Padding(padding: const EdgeInsets.all(16.0), child: Text(activeItems[index].title)));
+    return Card(child: Padding(padding: const EdgeInsets.all(16.0), child: Text(items[index].title)));
   }
 
   bool _contains(int index) {
-    return filter == null || filter == "" || activeItems[index].title.toLowerCase().contains(filter.toLowerCase());
+    return filter == null || filter == "" || items[index].title.toLowerCase().contains(filter.toLowerCase());
+  }
+
+  bool _isActive(int index) {
+    return items[index].isActive(_getActiveTags());
   }
 
   List<Tag> _getActiveTags() {
     return tags.where((tag) => tag.active).toList();
   }
 
-  void updateActiveItems() {
-    activeItems = items.where((item) => item.isActive(_getActiveTags())).toList();
-    activeItems.sort();
-    setState(() { });
+//  void updateActiveItems() {
+//    activeItems = items.where((item) => item.isActive(_getActiveTags())).toList();
+//    activeItems.sort();
+//    setState(() { });
+//  }
+
+  void _initItems() {
+    items = [];
+    Item item1 = Item("K-8 1", Level.K8);
+    item1.addTag(tags[0]);
+    Item item2 = Item("K-8 2", Level.K8);
+    Item item3 = Item("K-8 3", Level.K8);
+    Item item4 = Item("High 1", Level.High);
+    item4.addTag(tags[1]);
+    Item item5 = Item("High 2", Level.High);
+    item5.addTag(tags[2]);
+    Item item6 = Item("High 3", Level.High);
+    Item item7 = Item("Univ 1", Level.Univ);
+    item7.addTag(tags[3]);
+    Item item8 = Item("Univ 2", Level.Univ);
+    Item item9 = Item("Univ 3", Level.Univ);
+    Item item10 = Item("SAT 1", Level.SAT);
+    item10.addTag(tags[4]);
+    Item item11 = Item("SAT 2", Level.SAT);
+    Item item12 = Item("SAT 3", Level.SAT);
+    items.addAll([item1, item2, item3, item4, item5, item6, item7, item8, item9, item10, item11, item12]);
+  }
+
+  void _initTags() {
+    tags.add(Tag(id: 0, title: "algebra", active: false));
+    tags.add(Tag(id: 1, title: "differentiation",  active: false));
+    tags.add(Tag(id: 2, title: "integration",  active: false));
+    tags.add(Tag(id: 3, title: "calculus Ⅱ",  active: false));
+    tags.add(Tag(id: 4, title: "important",  active: false));
   }
 
   @override
   initState() {
     super.initState();
 
-    for (int i = 1; i <= 20; i++) {
-      Item item = Item(i.toString());
-      item.addTag(i % 2 == 0 ? "even" : "odd");
-      item.addTag(i % 3 == 0 ? "integration" : "differentiation");
-      items.add(item);
-    }
-
-    tags.add(Tag(id: 0, title: "even", active: false));
-    tags.add(Tag(id: 1, title: "odd",  active: false));
-    tags.add(Tag(id: 2, title: "differentiation",  active: false));
-    tags.add(Tag(id: 3, title: "integration",  active: false));
-    tags.add(Tag(id: 4, title: "algebra",  active: false));
+    _initTags();
+    _initItems();
 
     controller.addListener(() {
       setState(() {
@@ -243,13 +277,13 @@ class _BrowsePageState extends State<BrowsePage> {
                 child: SelectableTags(
                   activeColor: Colors.blueAccent,
                   tags: tags,
-                  onPressed: (tag) { updateActiveItems(); },
+                  onPressed: (tag) { setState(() {}); },
                 ),
               ),
               ListView.builder(
-                itemCount: activeItems.length,
+                itemCount: items.length,
                 itemBuilder: (BuildContext context, int index) {
-                  return _contains(index) ? _inkWell(_card(index)) : Container();
+                  return _contains(index) && _isActive(index) ? InkWell(onTap: (){ Navigator.push(context, MaterialPageRoute(builder: (context) => ProblemRoute(items[index]))); }, child: _card(index)) : Container();
                 },
                 physics: BouncingScrollPhysics(),
                 padding: EdgeInsets.all(0.0),
--- lib/routes.dart
+++ lib/routes.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tags/selectable_tags.dart';
+import 'pages.dart';
 
 class FormulaRoute extends StatelessWidget {
   @override
@@ -25,25 +27,63 @@ class FormulaRoute extends StatelessWidget {
 }
 
 class ProblemRoute extends StatelessWidget {
+  ProblemRoute(this.item);
+
+  final Item item;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Ploblem Route"),
+        title: Text(item.title),
       ),
       body: Center(
         child: Column(
           children: <Widget>[
-            Hero(
-              tag: "Tag 3",
-              child: Icon(Icons.mail, size: 50.0,),
+//            Hero(
+//              tag: "Tag 3",
+//              child: Icon(Icons.mail, size: 50.0,),
+//            ),
+            Padding(
+              padding: EdgeInsets.only(left: 20.0, top: 50.0, right: 20.0, bottom: 20.0),
+              child: Text(item.title, style: TextStyle(fontWeight: FontWeight.bold),),
             ),
-            RaisedButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: Text('Go back!'),
+            Text("Level: ${item.level.toString().split('.').last}"),
+            Padding(
+              padding: EdgeInsets.all(20.0),
+//              child: ListView.builder(
+//                itemCount: item.tags.length,
+//                itemBuilder: (BuildContext context, int index) {
+//                  return Text(item.tags[index].title);
+//                },
+//                physics: BouncingScrollPhysics(),
+//                padding: EdgeInsets.all(0.0),
+//                shrinkWrap: true,
+//              ),
+              child: SelectableTags(
+                activeColor: Colors.white,
+                textActiveColor: Colors.black,
+                tags: item.tags,
+                backgroundContainer: Colors.transparent,
+                alignment: MainAxisAlignment.start,
+//                onPressed: (tag) { setState(() {}); },
+              ),
             ),
+            Text(
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 12.0),
+            ),
+//            Padding(
+//              padding: EdgeInsets.all(20.0),
+//              child:
+//              FlatButton(
+//                onPressed: () {
+//                  Navigator.pop(context);
+//                },
+//                child: Text('Go back'),
+//              ),
+//            ),
           ],
         ),
       ),
@@ -53,9 +93,41 @@ class ProblemRoute extends StatelessWidget {
 
 class LevelRoute extends StatelessWidget {
 
-  LevelRoute({this.title, this.tag});
+  LevelRoute({this.title, this.tag, this.level});
 
   final String title, tag;
+  final Level level;
+
+//  InkWell _inkWell(Card card) {
+//    return InkWell(onTap: (){ Navigator.push(context, MaterialPageRoute(builder: (context) => ProblemRoute())); }, child: card);
+//  }
+
+//  List<Item> items = [];
+//
+//  void _initItems() {
+//    Item item1 = Item("K-8 1", Level.K8);
+//    item1.addTag("algebra");
+//    Item item2 = Item("K-8 2", Level.K8);
+//    Item item3 = Item("K-8 3", Level.K8);
+//    Item item4 = Item("High 1", Level.High);
+//    item4.addTag("differentiation");
+//    Item item5 = Item("High 2", Level.High);
+//    item5.addTag("integration");
+//    Item item6 = Item("High 3", Level.High);
+//    Item item7 = Item("Univ 1", Level.Univ);
+//    item7.addTag("calculus Ⅱ");
+//    Item item8 = Item("Univ 2", Level.Univ);
+//    Item item9 = Item("Univ 3", Level.Univ);
+//    Item item10 = Item("SAT 1", Level.SAT);
+//    item10.addTag("important");
+//    Item item11 = Item("SAT 2", Level.SAT);
+//    Item item12 = Item("SAT 3", Level.SAT);
+//    items.addAll([item1, item2, item3, item4, item5, item6, item7, item8, item9, item10, item11, item12]);
+//  }
+
+  Card _card(int index) {
+    return Card(child: Padding(padding: const EdgeInsets.all(16.0), child: Text(items[index].title)));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -83,6 +155,15 @@ class LevelRoute extends StatelessWidget {
                 "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
                 textAlign: TextAlign.center,
                 style: TextStyle(fontSize: 12.0),
+              ),
+              ListView.builder(
+                itemCount: items.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return items[index].level == level ? InkWell(onTap: (){ Navigator.push(context, MaterialPageRoute(builder: (context) => ProblemRoute(items[index]))); }, child: _card(index)) : Container();
+                },
+                physics: BouncingScrollPhysics(),
+                padding: EdgeInsets.all(0.0),
+                shrinkWrap: true,
               ),
             ],
           ),
