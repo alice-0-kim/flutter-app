d81536a0ff8ddf0c47ed614c0bea3b4d93f854e4
Alice Kim <alice828@mail.ubc.ca>
2019-06-10 14:40:35 -0700

    Use db for comments

---
+++ lib/comment.dart
@@ -0,0 +1,132 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'pages.dart';
+
+class CommentPage extends StatefulWidget {
+  CommentPage({Key key, this.title, this.item}) : super(key: key);
+
+  final String title;
+  final Item item;
+
+  @override
+  _CommentPageState createState() {
+    return _CommentPageState();
+  }
+}
+
+class _CommentPageState extends State<CommentPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Comments')),
+      body: _buildBody(context),
+    );
+  }
+
+  Widget _buildBody(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: Firestore.instance.collection('comment').snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) return LinearProgressIndicator();
+
+//        return _buildList(context, snapshot.data.documents);
+        return _buildStack(context, snapshot.data.documents);
+      },
+    );
+  }
+
+  Widget _buildStack(BuildContext context, List<DocumentSnapshot> snapshot) {
+    return Stack(
+      children: <Widget>[
+        _buildList(context, snapshot),
+        Positioned(
+          bottom: 0,
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            color: Colors.white,
+            child: TextField(
+              autofocus: true,
+              onSubmitted: (String submitted) {
+                setState(() {
+                  // TODO: update behaviour
+                });
+              },
+              decoration: InputDecoration(
+                contentPadding: const EdgeInsets.all(20.0),
+                hintText: "Add comment...",
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
+    return ListView(
+      padding: const EdgeInsets.all(5.0),
+      children: snapshot.map((data) => _buildListItem(context, data)).toList(),
+    );
+  }
+
+  Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
+    final record = Record.fromSnapshot(data);
+
+    return Padding(
+      key: ValueKey(record.user),
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 10.0),
+        decoration: BoxDecoration(
+          border: Border(
+            bottom: BorderSide(
+              color: Colors.black45,
+            ),
+          ),
+        ),
+//        child: ListTile(
+//          title: Text(record.user),
+//          trailing: Text(0.toString()),
+//          onTap: () => print(record),
+//        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(right: 10.0),
+                  child: Text(record.user, style: TextStyle(fontWeight: FontWeight.bold),),
+                ),
+                Text(new DateTime.now().toString()),
+              ],
+            ),
+            Text(record.text, textAlign: TextAlign.left,),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Record {
+  final String user;
+  final String text;
+//  final String timestamp;
+//  final int votes;
+  final DocumentReference reference;
+
+  Record.fromMap(Map<String, dynamic> map, {this.reference})
+      : assert(map['user'] != null),
+        assert(map['text'] != null),
+//        assert(map['timestamp'] != null),
+        user = map['user'],
+        text = map['text'];
+//        timestamp = map['timestamp'];
+
+  Record.fromSnapshot(DocumentSnapshot snapshot)
+      : this.fromMap(snapshot.data, reference: snapshot.reference);
+
+  @override
+  String toString() => "Record<$user>";
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,95 +1,68 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'pages.dart';
+//import 'package:flutter/services.dart';
+//import 'package:flutter_tags/selectable_tags.dart';
+//import 'dart:math';
+//import 'routes.dart';
 
 void main() => runApp(MyApp());
-
-//final dummySnapshot = [
-//  {"name": "Filip", "votes": 15},
-//  {"name": "Abraham", "votes": 14},
-//  {"name": "Richard", "votes": 11},
-//  {"name": "Ike", "votes": 10},
-//  {"name": "Justin", "votes": 1},
-//];
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Baby Names',
-      home: MyHomePage(),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
   @override
-  _MyHomePageState createState() {
-    return _MyHomePageState();
-  }
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Baby Name Votes')),
-      body: _buildBody(context),
-    );
-  }
-
-  Widget _buildBody(BuildContext context) {
-    return StreamBuilder<QuerySnapshot>(
-      stream: Firestore.instance.collection('baby').snapshots(),
-      builder: (context, snapshot) {
-        if (!snapshot.hasData) return LinearProgressIndicator();
-
-        return _buildList(context, snapshot.data.documents);
-      },
-    );
-  }
-
-  Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
-    return ListView(
-      padding: const EdgeInsets.only(top: 20.0),
-      children: snapshot.map((data) => _buildListItem(context, data)).toList(),
-    );
-  }
-
-  Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
-    final record = Record.fromSnapshot(data);
-
-    return Padding(
-      key: ValueKey(record.name),
-      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-      child: Container(
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.grey),
-          borderRadius: BorderRadius.circular(5.0),
-        ),
-        child: ListTile(
-          title: Text(record.name),
-          trailing: Text(record.votes.toString()),
-          onTap: () => print(record),
+      appBar: AppBar(
+        title: Text(widget.title),
+        actions: <Widget>[
+          popupMenuButton(),
+        ],
+      ),
+      body: Container(
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Container(
+                child: RaisedButton(
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => BrowsePage(title: "Browse")));
+                  },
+                  child: Text('Browse'),
+                ),
+              ),
+              Container(
+                child: RaisedButton(
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => LevelsPage(title: "Levels")));
+                  },
+                  child: Text('Levels'),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
-}
-
-class Record {
-  final String name;
-  final int votes;
-  final DocumentReference reference;
-
-  Record.fromMap(Map<String, dynamic> map, {this.reference})
-      : assert(map['name'] != null),
-        assert(map['votes'] != null),
-        name = map['name'],
-        votes = map['votes'];
-
-  Record.fromSnapshot(DocumentSnapshot snapshot)
-      : this.fromMap(snapshot.data, reference: snapshot.reference);
-
-  @override
-  String toString() => "Record<$name:$votes>";
 }
